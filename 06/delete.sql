61a709ab64d95758ebaf423d55d5263bd8620f81
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2022-02-14 00:07:08 +0800

    CODE: CRUD Exercise Delete Solution

---
+++ 06/delete.sql
@@ -0,0 +1,36 @@
+use shirts_db;
+--
+SELECT *
+FROM shirts;
+--
+SELECT *
+FROM shirts
+WHERE last_worn = 200;
+--
+DELETE FROM shirts
+WHERE last_worn = 200;
+--
+SELECT *
+FROM shirts
+WHERE article = 'tank top';
+--
+DELETE FROM shirts
+WHERE article = 'tank top';
+--
+SELECT *
+FROM shirts
+WHERE article = 'tank top';
+--
+SELECT *
+FROM shirts;
+--
+DELETE FROM shirts;
+--
+SELECT *
+FROM shirts;
+--
+DROP TABLE shirts;
+--
+show tables;
+--
+DESC shirts;
---
+++ 06/read.sql
@@ -0,0 +1,16 @@
+use shirts_db;
+--
+SELECT article,
+    color
+FROM shirts;
+--
+SELECT *
+FROM shirts
+WHERE shirt_size = 'M';
+--
+SELECT article,
+    color,
+    shirt_size,
+    last_worn
+FROM shirts
+WHERE shirt_size = 'M';
